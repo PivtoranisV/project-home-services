4c767a316bf751462f7f5628ea5287c452fa85ee
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-03-19 23:07:36 +0400

    Added layout and styles to cart items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,18 +2,25 @@ import React, { useContext } from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
+import CartItem from './CartItem';
 
 const Cart = (props) => {
   const ctx = useContext(CartContext);
   const totalAmount = `$${ctx.totalAmount.toFixed(2)}`;
   const hasServices = ctx.services.length > 0;
+  const totalHours = ctx.services.reduce((prev, current) => {
+    return prev + current.hours;
+  }, 0);
 
   const cartItems = (
     <ul className={styles['cart-items']}>
       {ctx.services.map((service) => (
-        <li>
-          {service.name} {service.hours}
-        </li>
+        <CartItem
+          key={service.id}
+          name={service.name}
+          price={service.price}
+          hours={service.hours}
+        />
       ))}
     </ul>
   );
@@ -22,8 +29,14 @@ const Cart = (props) => {
     <Modal onHideCart={props.onHideCart}>
       {cartItems}
       <div className={styles.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
+        <div>
+          <span>Total Hours</span>
+          <span className={styles.amount}>{totalHours}</span>
+        </div>
+        <div>
+          <span>Total Amount</span>
+          <span className={styles.amount}>{totalAmount}</span>
+        </div>
       </div>
       <div className={styles.actions}>
         <button className={styles.close} onClick={props.onHideCart}>
--- src/components/Services/dummyServices.js
+++ src/components/Services/dummyServices.js
@@ -4,35 +4,35 @@ const DUMMY_SERVICES = [
     name: 'General Home Cleaning',
     description:
       'Vacuuming the carpet, mopping the floors, cleaning the windows, and so on.',
-    price: 15.99,
+    price: 7.99,
   },
   {
     id: 's2',
     name: 'Deep Home Cleaning',
     description:
       'Professionals will also clean air filters, ceiling fans, wash curtains, and more.',
-    price: 19.99,
+    price: 10.99,
   },
   {
     id: 's3',
     name: 'Bedroom Cleaning',
     description:
       'Our professionals will change your bed sheets if necessary, dust all the wardrobes, and ensure that your bedroom is fresh after they leave.',
-    price: 7.5,
+    price: 3.5,
   },
   {
     id: 's4',
     name: 'Kitchen Cleaning',
     description:
       'Cleaning professionals will scrub cabinets and countertops and clean all the appliances, such as the refrigerator.',
-    price: 11.99,
+    price: 5.99,
   },
   {
     id: 's5',
     name: 'Bathroom Cleaning',
     description:
       'The bathroom cleaning includes scrubbing sinks and bathtubs, disposing of waste in your bathroom, and cleaning the mats',
-    price: 9.5,
+    price: 6.5,
   },
 ];
 
