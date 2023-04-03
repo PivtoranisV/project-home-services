349af203f5899e96ef6d98ba526e8f21a5e944de
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-04-03 23:33:56 +0400

    Fixed services fetching with loading and error

--- src/components/Services/ListServices.js
+++ src/components/Services/ListServices.js
@@ -5,12 +5,21 @@ import ServiceItem from './ServiceItem';
 
 const ListServices = () => {
   const [services, setServices] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       'https://home-services-40fc3-default-rtdb.firebaseio.com/services.json'
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch services.');
+        }
+        return response.json();
+      })
+      .catch((error) => setError(error))
       .then((data) => {
         const loadedData = [];
         for (const key in data) {
@@ -22,8 +31,25 @@ const ListServices = () => {
           });
         }
         setServices(loadedData);
+        setIsLoading(false);
       });
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className={styles.error}>
+        <p>{error.message}</p>
+      </section>
+    );
+  }
 
   const servicesList = services.map((service) => (
     <ServiceItem
