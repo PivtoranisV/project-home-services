58adda6040b5b559c28e092d3574d0df24e9b41f
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-04-03 22:46:46 +0400

    Added fetching services vis HTTP

--- src/components/Services/ListServices.js
+++ src/components/Services/ListServices.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../UI/Card';
-import DUMMY_SERVICES from './dummyServices';
 import styles from './ListServices.module.css';
 import ServiceItem from './ServiceItem';
 
 const ListServices = () => {
-  const servicesList = DUMMY_SERVICES.map((service) => (
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      'https://home-services-40fc3-default-rtdb.firebaseio.com/services.json'
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const loadedData = [];
+        for (const key in data) {
+          loadedData.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        setServices(loadedData);
+      });
+  }, []);
+
+  const servicesList = services.map((service) => (
     <ServiceItem
       id={service.id}
       name={service.name}
