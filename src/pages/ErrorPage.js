12421e3799fd06f6a2273a25e9b70796978a7f15
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2023-04-28 12:11:39 +0400

    Fixed Error page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { useRouteError } from 'react-router-dom';
 import ErrorContent from '../components/Content/ErrorContent';
+import Header from '../components/Header/Header';
 
 const ErrorPage = () => {
-  const title = 'An Error ocurred!';
-  const message = 'Something happen';
+  const error = useRouteError();
 
+  let title = 'An error occurred!';
+  let message = 'Something went wrong';
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = 'Not found!';
+    message = 'Could not find page';
+  }
   return (
-    <ErrorContent title={title}>
-      <p>{message}</p>
-    </ErrorContent>
+    <>
+      <Header />
+      <ErrorContent title={title}>
+        <p>{message}</p>
+      </ErrorContent>
+    </>
   );
 };
 
